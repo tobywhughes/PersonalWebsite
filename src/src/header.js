49dc54fe691aec7684263df5e773a9e80924d630
Toby Hughes <tobywhughes@gmail.com>
2017-08-27 14:12:22 -0700

    Test content (header and body)

---
+++ src/src/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './header.css'
+
+class Header extends React.Component{
+    render() {
+        return <h1 className="Header-Title">HEADER CLASS</h1>;
+    }
+}
+
+export default Header;
---
+++ src/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './header';
+import './index.css'
+
+const App = 
+(   <div>
+        <Header />
+        <p>cout &lt;&lt; "A Quick Introduction" &lt;&lt; endl;</p>
+    </div>
+);
+
+
+ReactDOM.render(App, document.getElementById('root'));
