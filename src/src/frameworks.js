6ecaf45cf023ac095061893f1de2928c9a24c0d3
Toby Hughes <tobywhughes@gmail.com>
2017-11-15 15:07:01 -0800

    Added frameworks.

---
+++ src/src/frameworks.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './frameworks.css'
+
+class Frameworks extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            name: props.name,
+            tag: props.tag,
+            side: props.side,
+            image: props.image,
+        }
+    }
+    render() {
+        return <div id={this.state.tag} className={"Frameworks" + this.state.side}>
+                    <div id="imagediv">
+                        <img id={this.state.tag + "Image"} src={this.state.image} alt=''/>
+                    </div>
+                    <div id="textdiv">
+                        <h1 >{this.state.name}</h1>
+                    </div>
+                </div>;
+    }
+}
+
+export default Frameworks;
--- src/src/index.js
+++ src/src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './header';
 import Languages from './languages';
+import Frameworks from './frameworks';
 import './index.css'
 import pyimage from './images/python.png'
 import rustimage from './images/rust.png'
 import csharpimage from './images/csharp.png'
 import cppimage from './images/cpp.png'
 import jsimage from './images/js.png'
+import reactimage from './images/react.png'
+import xamarinimage from './images/xamarin.png'
+import aspnetimage from './images/aspnet.png'
+import djangoimage from './images/django.png'
+import nunitimage from './images/nunit.png'
+import pyqtimage from './images/pyqt.png'
 
 const App = 
 (   <div>
@@ -29,7 +36,8 @@ const App =
              I am also interested in partcipating in the software community as well. This can be seen by my
              involvement in the open source community, speaking engagements at developer events, and lending support
              on help forums such as Stack Overflow and reddit.
-        <br /><br /> &gt;&gt; ./Languages       
+        <br /><br /> &gt;&gt; ./Languages <br/><br/>
+        PRINTING LANGUAGES...      
         </p>
         <Languages name = "Python" tag= "Python" image= {pyimage}/>
         <Languages name = "Rust" tag= "Rust" image= {rustimage}/>
@@ -37,9 +45,26 @@ const App =
         <Languages name = "C/C++" tag= "CPP" image= {cppimage}/>
         <Languages name = "JavaScript" tag= "JavaScript" image= {jsimage}/>
         <p>
-        Other Used Languages: Java, HTML/CSS, Xaml, MASM, GASM<br/><br/>
+        Other Languages: Java, HTML/CSS, Xaml, MASM, GASM<br/><br/>
         &gt;&gt; ./Frameworks
-        <br/>
+        <br/><br/>
+        PRINTING FRAMEWORKS...
+        </p>
+        <Frameworks name = "React" tag= "React" side= "Left" image= {reactimage} />
+        <Frameworks name = "Xamarin" tag= "Xamarin" side= "Right" image= {xamarinimage} />
+        <Frameworks name = "ASP.NET" tag= "ASPNET" side= "Left" image= {aspnetimage} />
+        <Frameworks name = "Django" tag= "Django" side= "Right" image= {djangoimage} />
+        <Frameworks name = "NUnit" tag= "NUnit" side= "Left" image= {nunitimage} />
+        <Frameworks name = "PyQt" tag= "PyQt" side= "Right" image= {pyqtimage} />
+        <p>
+        Other Frameworks: Prism, Entity, Tkinter, Pygame<br/><br/>
+        &gt;&gt; ./CurrentProjects <br/><br/>
+        SCANNING GITHUB FOR OPEN SOURCE PROJECTS. PLEASE WAIT...<br/>
+        FOUND (3) CURRENT OPEN SOURCE PROJECTS. PRINT PROJECT DETAILS? [Y]es or [N]o?<br/><br/>
+        &gt;&gt; sure<br/><br/>
+        INVALID INPUT. PLEASE ENTER [Y] OR [N].<br/><br/>
+        &gt;&gt; y<br/><br/>
+        PRINTING PROJECT DETAILS...
         </p>
     </div>
 );
